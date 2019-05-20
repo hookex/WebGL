b256417845cd219839d4a689dfaf57d704104540
hookex <cookmycode@gmail.com>
2019-05-20 14:13:20 +0800

    luma

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WebGL
 3d 性能测试
+
+
+### 对比
+
+Luma.gl vs 3d.js
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const {resolve} = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CONFIG = {
+    mode: 'development',
+
+    entry: {
+        app: resolve('./luma.js')
+    },
+
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Instancing',
+        template: './index.html'
+    })]
+};
+
+// This line enables bundling against src in this repo rather than installed module
+module.exports = env => (env ? require('./webpack.config.local')(CONFIG)(env) : CONFIG);
