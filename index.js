ba844cbee9b85fadbd79f358fb72e8ca4c3548f4
hookex <cookmycode@gmail.com>
2019-06-06 14:39:41 +0800

    es6 module

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import * as THREE from "./static/three";
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+import {Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh, WebGLRenderer} from './static/three';
 
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({color: 0x00ff00});
+const cube = new Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
 
 
 function animate() {
