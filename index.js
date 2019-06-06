b9de9cfcdb53871a439dd5d9b6417ac7661f62e0
hookex <cookmycode@gmail.com>
2019-06-06 17:44:40 +0800

    import gltf model

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-import {Scene, PerspectiveCamera, Geometry, Vector3, Line, LineBasicMaterial, Mesh, WebGLRenderer} from './static/three';
+import {
+    Scene,
+    PerspectiveCamera,
+    Geometry,
+    Vector3,
+    Line,
+    LineBasicMaterial,
+    Mesh,
+    WebGLRenderer
+} from './static/three';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+
+const loader = new GLTFLoader()
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set( 0, 0, 50 );
-camera.lookAt( 0, 0, 0 );
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
-const material = new LineBasicMaterial( { color: 0x0000ff } );;
-
-const geometry = new Geometry();
-geometry.vertices.push(new Vector3( -10, 0, 0) );
-geometry.vertices.push(new Vector3( 0, 10, 0) );
-geometry.vertices.push(new Vector3( 10, 0, 0) );
-
-const line = new Line( geometry, material );
-scene.add(line);
+loader.load('./static/corsa_finale/scene.gltf', function (gltf) {
+    scene.add(gltf.scene);
+}, undefined, function (error) {
+    console.error(error);
+});
 
 const renderer = new WebGLRenderer();
 
