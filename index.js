e7a6321b1dcc5444fda77ed3a12d3062636d2ef6
hookex <cookmycode@gmail.com>
2019-06-06 16:36:10 +0800

    WebGL 2

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-import {Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh, WebGLRenderer} from './static/three';
-
+import {Scene, PerspectiveCamera, BoxGeometry, ShaderMaterial, Mesh, WebGLRenderer} from './static/three';
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 
 const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({color: 0x00ff00});
+const material = new ShaderMaterial({
+    vertexShader: document.getElementById('vs').textContent.trim(),
+    fragmentShader: document.getElementById('fs').textContent.trim()
+});
 const cube = new Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('webgl2');
+const renderer = new WebGLRenderer({canvas: canvas, context: context});
 
-const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -21,8 +25,8 @@ document.body.appendChild(renderer.domElement);
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 }
