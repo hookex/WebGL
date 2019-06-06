d6d55298a892f693be4ff82bb585f1108cc8b8d2
hookex <cookmycode@gmail.com>
2019-06-06 14:34:20 +0800

    cube animate done

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 import * as THREE from "./static/three";
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+}
+
+animate();
