61b954a32b4e24bbce666f03c54ac75163eb46f4
hookex <cookmycode@gmail.com>
2019-06-10 16:06:13 +0800

    threejs-fundamentals.html

--- index.js
+++ index.js
@@ -1,38 +1,69 @@
-import {
-    Scene,
-    PerspectiveCamera,
-    Geometry,
-    Vector3,
-    Line,
-    LineBasicMaterial,
-    Mesh,
-    WebGLRenderer
-} from './static/three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
-
-const loader = new GLTFLoader()
-
-const scene = new Scene();
-const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 100);
-camera.lookAt(0, 0, 0);
-
-loader.load('./static/corsa_finale/scene.gltf', function (gltf) {
-    scene.add(gltf.scene);
-}, undefined, function (error) {
-    console.error(error);
-});
-
-const renderer = new WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+import * as THREE from 'three';
 
 
-function animate() {
-    requestAnimationFrame(animate);
+
+function main() {
+
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({canvas});
+
+
+    const fov = 75;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
+
+    const scene = new THREE.Scene();
+
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+
+    function makeInstance(geometry, color, x) {
+        const material = new THREE.MeshPhongMaterial({color});
+
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        cube.position.x = x;
+
+        return cube;
+    }
+
+
+    const cubes = [
+        makeInstance(geometry, 0x44aa88,  0),
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0xaa8844,  2),
+    ];
+
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
 
     renderer.render(scene, camera);
+
+    function render(time) {
+        time *= 0.001;
+        cubes.forEach((cube, ndx) => {
+            const speed = 1 + ndx * .1;
+            const rot = time * speed;
+            cube.rotation.x = rot;
+            cube.rotation.y = rot;
+        });
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render)
+    }
+
+    requestAnimationFrame(render)
 }
 
-animate();
+main()
