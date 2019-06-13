b0e8a862d0dcc7fd9d3b1114973368cae388a1fb
hookex <cookmycode@gmail.com>
2019-06-13 20:42:57 +0800

    clean index

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// import "./solar"
-// import "./gltf"
-// import "./buffer-geometry"
-// import "./instanced-mesh"
 // import "./instancing-with-three-1.1"
 // import "./instancing-with-three-1.2"
 // import "./instancing-with-three-1.3"
+
+
+// import "./instanced-mesh"
+// import "./solar"
+// import "./gltf"
+// import "./buffer-geometry"
 import "./gltf-instancing"
